f9c8222e4093f43256ff0873f50990ae421aafb4
小川鼓太郎 <kotaroogawa@ogawatsudzumitarounoMacBook-puro.local>
2019-12-08 16:09:41 +0900

    くそ

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once(dirname(__FILE__).'/vendor/autoload.php');
+require_once(dirname(__FILE__)."/vendor/autoload.php");
 
-$inputString = file_get_contents('php://input');
+$inputString = file_get_contents("php://input");
 error_log($inputString);
 
 ?>
